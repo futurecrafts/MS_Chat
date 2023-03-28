f7a48ecd8b9713140e763f5063a50cc3ab1e1df3
YB <yb@yb.com>
2023-03-28 15:06:05 +1000

    first commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+console.log('Welcome to the LangChain.js tutorial by LangChainers.');
+
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import indexRouter from "./routes/index.js"
+import usersRouter from "./routes/users.js"
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+//app.use(express.urlencoded());
+app.use(
+    express.urlencoded({ extended: true }),
+    cors({
+        origin: process.env.CLIENT_URL,
+        methods: ["GET", "POST"],
+        credentials: true
+    })
+);
+
+app.use('/', indexRouter);
+app.use('/api', usersRouter); 
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.send('home!')
+});
+
+router.get('/about', (req, res) => {
+  res.send('about!')
+});
+
+export default router;
